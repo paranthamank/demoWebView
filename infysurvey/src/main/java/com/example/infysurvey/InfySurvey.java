69f9ab618afa5598220c9547321b18737272c105
paranthamank <paranthamanks1999@gmail.com>
2021-11-08 16:32:22 +0530

    first

---
+++ infysurvey/src/main/java/com/example/infysurvey/InfySurvey.java
@@ -0,0 +1,14 @@
+package com.example.infysurvey;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class InfySurvey {
+    public  void callWebview(WebView webView){
+//        WebView webView1=webView;
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("https://surveys.infosysapps.com/r/a/OyYFznqvwezV");
+
+    }
+}
